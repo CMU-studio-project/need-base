44eda70f2c813bfbbbef416562639432cf317cc0
Kyumin-Park <pkm9403@gmail.com>
2023-01-27 13:49:20 -0500

    feat: add nlp sentiment roberta model

---
+++ nlp/controller.py
@@ -0,0 +1,14 @@
+from nlp.models import load_model
+import json
+
+
+class NLPTaskController:
+    def __init__(self, task: str = "sentiment", model: str = "roberta"):
+        self.model = load_model(task, model)
+    
+    def predict(self, text: str) -> bytes:
+        pred_label, pred_prob = self.model(text)    # (label[str], prob[float])
+        pred_data = json.dumps({pred_label: pred_prob}, ensure_ascii=False)
+        pred_data_bytes = pred_data.encode("utf-8")
+        
+        return pred_data_bytes
---
+++ nlp/models/base.py
@@ -0,0 +1,13 @@
+from typing import Any, Dict, Tuple
+
+class ModelBase:
+    def encode_text(self, text: str) -> Dict[str, Any]:
+        pass
+
+    def predict_label(self, encoded_text: Dict[str, Any]) -> Tuple[str, float]:
+        pass
+    
+    def __call__(self, text: str) -> Tuple[str, float]:
+        encoded_text = self.encode_text(text)
+        prediction = self.predict_label(encoded_text)
+        return prediction
