2a1dfb88610e9b8f9e45bed617528267fc8268f4
Kyumin-Park <pkm9403@gmail.com>
2023-01-31 17:47:20 -0500

    fix: split stt/nlp

--- app/stt/controller.py
+++ app/stt/controller.py
@@ -1,4 +1,6 @@
+import os
 import json
+from tempfile import NamedTemporaryFile
 from pathlib import Path
 from typing import Any
 
@@ -10,50 +12,46 @@ from needpubsub.publish import publish_message
 NLP_ROOT = Path(__file__).parent
 
 
-class NLPTaskController:
+class STTController:
+    task = "automatic-speech-recognition"
+    
     def __init__(
         self,
-        task: str = "sentiment-analysis",
-        model: str = "roberta",
+        model: str = "wav2vec2-large",
         project_id: str = "iitp-class-team-4",
         topic_id: str = "stt-text"
     ):
         self.project_id = project_id
         self.topic_id = topic_id
-        model_config = self.load_model(task, model)
-        self.data_type = model_config["input_type"]
-        self.pipeline = pipeline(task, model=model_config["model"])
-        self.label_map = model_config.get("label_map")
+        model_name = self.load_model(model)
+        self.pipeline = pipeline(self.task, model=model_name)
 
-    @staticmethod
-    def load_model(task: str, model: str) -> Any:
+    def load_model(self, model: str) -> Any:
         with open(NLP_ROOT / "model_card.json", "r", encoding="utf-8") as f:
             model_card = json.load(f)
-        task_card = model_card.get(task)
-        if task_card is None:
-            raise ValueError(f"Task {task} not supported")
+        model_name = model_card[self.task].get(model)
+        if model_name is None:
+            raise ValueError(f"Model {model} not supported for task {self.task}")
 
-        model_config = task_card.get(model)
-        if model_config is None:
-            raise ValueError(f"Model {model} not supported for task {task}")
-
-        return model_config
-
-    def inference(self, text: str) -> bytes:
-        prediction = self.pipeline(text)[0]
-        if self.label_map is not None:
-            prediction["label"] = self.label_map[prediction["label"]]
-        pred_data = json.dumps(prediction, ensure_ascii=False)
-        pred_data_bytes = pred_data.encode("utf-8")
-
-        return pred_data_bytes
+        return model_name
+    
+    def inference(self, audio: bytes) -> bytes:
+        with NamedTemporaryFile(suffix=".wav", delete=False) as fp:
+            fp.write(audio)
+            temp_audio_name = fp.name
+        
+        prediction = self.pipeline(temp_audio_name)["text"]
+        prediction_bytes = prediction.encode("utf-8")
+        
+        os.remove(temp_audio_name)
+        
+        return prediction_bytes
 
     def eventsub(self, subscription_id: str) -> None:
         subscribe_message_async(self.project_id, subscription_id, self.sub_callback)
 
     def sub_callback(self, message: bytes, **kwargs) -> None:  # type: ignore[no-untyped-def]
-        message_text = message.decode("utf-8")
-        prediction = self.inference(message_text)
+        prediction = self.inference(message)
         
         print(f"Publishing {prediction.decode('utf-8')}")
         
@@ -70,12 +68,11 @@ if __name__ == "__main__":
     import argparse
     
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--task", type=str, help="NLP task")
     parser.add_argument("-m", "--model", type=str, help="NLP model for task in model card")
     parser.add_argument("-p", "--project_id", type=str, help="Google Pub/Sub Project ID")
     parser.add_argument("-s", "--subscription_id", type=str, help="Google Pub/Sub subscription ID")
     parser.add_argument("--topic_id", type=str, help="Google Pub/Sub Topic ID")
     args = parser.parse_args()
     
-    controller = NLPTaskController(args.task, args.model, project_id=args.project_id, topic_id=args.topic_id)
+    controller = STTController(args.model, project_id=args.project_id, topic_id=args.topic_id)
     controller.eventsub(args.subscription_id)
