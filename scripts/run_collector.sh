a3d871f12c0db32db36e47b3bcede276d520a564
Kyumin-Park <pkm9403@gmail.com>
2023-02-01 22:39:20 -0500

    feat: add scripts

---
+++ scripts/run_collector.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PROJECT_ROOT=$(pwd)
+export GOOGLE_APPLICATION_CREDENTIAL="${PROJECT_ROOT}/credentials/iitp-class-team-4-4fb491fb905a.json"
+
+PROJECT_ID=${PROJECT_ID:-'iitp-class-team-4'}
+TOPIC_ID=${TOPIC_ID:-'command'}
+SUBSCRIPTION_ID=${SUBSCRIPTION_ID:-'collector-sub'}
+
+cd "${PROJECT_ROOT}/app/collector"
+
+python controller.py --project_id ${PROJECT_ID} --topic_id ${TOPIC_ID} --subscription_id ${SUBSCRIPTION_ID}
---
+++ scripts/run_stt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PROJECT_ROOT=$(pwd)
+export GOOGLE_APPLICATION_CREDENTIAL="${PROJECT_ROOT}/credentials/iitp-class-team-4-4fb491fb905a.json"
+
+PROJECT_ID=${PROJECT_ID:-'iitp-class-team-4'}
+TOPIC_ID=${TOPIC_ID:-'command'}
+SUBSCRIPTION_ID=${SUBSCRIPTION_ID:-'collector-sub'}
+TASK=${TASK:-'automatic-speech-analysis'}
+MODEL=${MODEL:-'wav2vec2-large'}
+
+
+cd "${PROJECT_ROOT}/app/stt"
+
+python controller.py --project_id ${PROJECT_ID} --topic_id ${TOPIC_ID} --subscription_id ${SUBSCRIPTION_ID} --task ${TASK} --model ${MODEL}
