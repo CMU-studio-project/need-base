ad3c8a090d7d4653bfa16377fc0ef9bdac3fcf3c
Kyumin-Park <pkm9403@gmail.com>
2023-02-01 23:13:22 -0500

    fix: fix scripts

--- scripts/run_nlp.sh
+++ scripts/run_nlp.sh
@@ -4,8 +4,8 @@ PROJECT_ROOT=$(pwd)
 export GOOGLE_APPLICATION_CREDENTIAL="${PROJECT_ROOT}/credentials/iitp-class-team-4-4fb491fb905a.json"
 
 PROJECT_ID=${PROJECT_ID:-'iitp-class-team-4'}
-TOPIC_ID=${TOPIC_ID:-'command'}
-SUBSCRIPTION_ID=${SUBSCRIPTION_ID:-'collector-sub'}
+TOPIC_ID=${TOPIC_ID:-'collector'}
+SUBSCRIPTION_ID=${SUBSCRIPTION_ID:-'stt-text-sentiment'}
 TASK=${TASK:-'sentiment-analysis'}
 MODEL=${MODEL:-'roberta'}
 
--- scripts/run_stt.sh
+++ scripts/run_stt.sh
@@ -4,12 +4,12 @@ PROJECT_ROOT=$(pwd)
 export GOOGLE_APPLICATION_CREDENTIAL="${PROJECT_ROOT}/credentials/iitp-class-team-4-4fb491fb905a.json"
 
 PROJECT_ID=${PROJECT_ID:-'iitp-class-team-4'}
-TOPIC_ID=${TOPIC_ID:-'command'}
-SUBSCRIPTION_ID=${SUBSCRIPTION_ID:-'collector-sub'}
+TOPIC_ID=${TOPIC_ID:-'stt-text'}
+SUBSCRIPTION_ID=${SUBSCRIPTION_ID:-'stt-text-sentiment'}
 TASK=${TASK:-'automatic-speech-analysis'}
 MODEL=${MODEL:-'wav2vec2-large'}
 
 
 cd "${PROJECT_ROOT}/app/stt"
 
-python controller.py --project_id ${PROJECT_ID} --topic_id ${TOPIC_ID} --subscription_id ${SUBSCRIPTION_ID} --task ${TASK} --model ${MODEL}
+python controller.py --project_id ${PROJECT_ID} --topic_id ${TOPIC_ID} --subscription_id ${SUBSCRIPTION_ID} --model ${MODEL}
